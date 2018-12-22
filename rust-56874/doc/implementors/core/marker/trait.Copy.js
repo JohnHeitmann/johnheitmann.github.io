726fe8d92a367ba2adc636d029483e84c228caaa
John Heitmann <jheitmann@gmail.com>
2018-12-22 13:29:15 -0800

    Update for rust-56874

---
+++ rust-56874/doc/implementors/core/marker/trait.Copy.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["alloc"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"alloc/alloc/struct.Global.html\" title=\"struct alloc::alloc::Global\">Global</a>",synthetic:false,types:["alloc::alloc::Global"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"alloc/string/enum.ParseError.html\" title=\"enum alloc::string::ParseError\">ParseError</a>",synthetic:false,types:["alloc::string::ParseError"]},];
+implementors["core"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
